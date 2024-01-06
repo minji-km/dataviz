161cc423547408576aef83cd0f60b1d7dc40c890
minji-km <minji.kmk@gmail.com>
2024-01-06 23:00:04 +0100

    Update dataviz.py

--- dataviz.py
+++ dataviz.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from figures import fig1, fig2, fig3, fig4, fig5, fig6, fig7, fig8, fig9
+from figures import fig1, fig2, fig3, fig4, fig5, fig6, fig7, fig8, fig9, fig10
 
 tabs = ["Multivariate Analysis", "Parental Level of Education in comparison with ethnicity and gender", "Bar Charts", "Test Score Comparison with stats", "Scatter Plot of math and writing scores", "Box Plot of scores", "Heatmap of gender and parental level of education", "Pairplot", "Ethnicity Analysis", "Distplot"]
 selected_tab = st.sidebar.selectbox("Select Visualization", tabs)
@@ -22,5 +22,5 @@ elif selected_tab == "Pairplot":
     fig8()
 elif selected_tab == "Ethnicity Analysis":
     fig9()
-elif selected_tab == "Distplot"
+elif selected_tab == "Distplot":
     fig10()
